514b2eca4f3527d24e2a36a0b56464cbce24a844
cristiaanx899 <cristiaanx899@gmail.com>
2025-10-17 21:56:38 -0600

    12356

--- InitializrSpringbootProject/src/main/java/com/example/InventarioPlus/service/UsuarioService.java
+++ InitializrSpringbootProject/src/main/java/com/example/InventarioPlus/service/UsuarioService.java
@@ -1,5 +1,36 @@
 package com.example.InventarioPlus.service;
+import com.example.InventarioPlus.model.*;
+import com.example.InventarioPlus.controller.*;
+import com.example.InventarioPlus.repository.*;
+import com.example.InventarioPlus.service.*;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@ Service
 public class UsuarioService {
+private final UsuarioRepository usuarioRepository;
 
+    // Inyección de dependencias por constructor
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> obtenerUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario guardarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void eliminarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
 }
