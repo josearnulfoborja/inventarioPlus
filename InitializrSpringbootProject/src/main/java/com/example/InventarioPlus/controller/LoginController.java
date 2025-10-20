aca1061b84631971743f1c4f75cbc9fb5492385d
Jose Borja <josearnulfoborja@hotmail.com>
2025-10-19 18:51:53 -0600

    Cambios locales
    Cambios locales JB

--- InitializrSpringbootProject/src/main/java/com/example/InventarioPlus/controller/LoginController.java
+++ InitializrSpringbootProject/src/main/java/com/example/InventarioPlus/controller/LoginController.java
@@ -1,4 +1,4 @@
-﻿package com.example.InventarioPlus.controller;
+package com.example.InventarioPlus.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
